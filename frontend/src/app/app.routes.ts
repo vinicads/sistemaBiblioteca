92d4c58a21976cb693bd07a4de0ec8e7769e63fd
Vinicius Augusto <vinicim.augusto@outlook.com>
2025-01-19 21:35:11 -0300

    Enviando ultimas updt

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,8 +2,13 @@ import { Routes } from '@angular/router';
 import { AuthorListComponent } from './author-list/author-list.component';
 import { BookListComponent } from './book-list/book-list.component';
 import { ChatComponent } from './websocket/chat.component';
+import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
+    {
+        path: "",
+        component: HomeComponent
+    },
     {
         path: "authors",
         component: AuthorListComponent,
